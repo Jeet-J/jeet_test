9decb2918064235ae7f5f7b9d9eb8d16455142f0
jeet0892070715 <jeet.j@hotmail.com>
2017-10-23 16:29:47 +0100

    Update script1.ps1

--- script1.ps1
+++ script1.ps1
@@ -4,4 +4,4 @@ cd\
 Invoke-Sqlcmd
 Set-Location SQLSERVER:\SQL\agtest\DEFAULT\databases\SSISDB
 Invoke-Sqlcmd -Query "SELECT GETDATE() AS TimeOfQuery;" -Verbose
-Invoke-Sqlcmd -Query "DECLARE @ProjectBinary as varbinary(max);DECLARE @operation_id as bigint ; Set @ProjectBinary = (SELECT * FROM OPENROWSET(BULK 'C:\Test\Share\ssis\*.ispac', SINGLE_BLOB) as BinaryData); Exec catalog.deploy_project @folder_name = 'jeet_test', @project_name = 'jeet_test', @Project_Stream = @ProjectBinary, @operation_id = @operation_id out;" -Verbose
+Invoke-Sqlcmd -Query "DECLARE @ProjectBinary as varbinary(max);DECLARE @operation_id as bigint ; Set @ProjectBinary = (SELECT * FROM OPENROWSET(BULK 'C:\Test\Share\ssis\jeet_test.ispac', SINGLE_BLOB) as BinaryData); Exec catalog.deploy_project @folder_name = 'jeet_test', @project_name = 'jeet_test', @Project_Stream = @ProjectBinary, @operation_id = @operation_id out;" -Verbose
